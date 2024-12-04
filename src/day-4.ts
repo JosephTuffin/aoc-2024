83c49293d92ade792e3ef0452fe23697f87abee6
Joseph Tuffin <joseph.tuffin@atradius.com>
2024-12-04 10:56:42 +0000

    added day 4

---
+++ src/day-4.ts
@@ -0,0 +1,46 @@
+const input = `MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX`;
+
+export const wordSearchPart1 = () => {
+  const grid = input.split("\n").map((row) => row.split(""));
+  const total = grid.reduce(
+    (count, row, x) =>
+      count +
+      row.reduce((rowCount, _, y) => {
+        const xmas1 = +["XMAS", "SAMX"].includes(grid[x]?.[y] + grid[x + 1]?.[y] + grid[x + 2]?.[y] + grid[x + 3]?.[y]);
+        const xmas2 = +["XMAS", "SAMX"].includes(grid[x]?.[y] + grid[x]?.[y + 1] + grid[x]?.[y + 2] + grid[x]?.[y + 3]);
+        const xmas3 = +["XMAS", "SAMX"].includes(
+          grid[x]?.[y] + grid[x + 1]?.[y + 1] + grid[x + 2]?.[y + 2] + grid[x + 3]?.[y + 3],
+        );
+        const xmas4 = +["XMAS", "SAMX"].includes(
+          grid[x]?.[y] + grid[x + 1]?.[y - 1] + grid[x + 2]?.[y - 2] + grid[x + 3]?.[y - 3],
+        );
+        return rowCount + xmas1 + xmas2 + xmas3 + xmas4;
+      }, 0),
+    0,
+  );
+  return total;
+};
+
+export const wordSearchPart2 = () => {
+  const grid = input.split("\n").map((row) => row.split(""));
+  const total = grid.reduce(
+    (count, row, x) =>
+      count +
+      row.reduce((rowCount, _, y) => {
+        const mas1 = ["MAS", "SAM"].includes(grid[x - 1]?.[y - 1] + grid[x][y] + grid[x + 1]?.[y + 1]);
+        const mas2 = ["MAS", "SAM"].includes(grid[x + 1]?.[y - 1] + grid[x][y] + grid[x - 1]?.[y + 1]);
+        return rowCount + (mas1 && mas2 ? 1 : 0);
+      }, 0),
+    0,
+  );
+  return total;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { matchTotalPart1, matchTotalPart2 } from "./day-1.js";
 import { safeReportsPart1, safeReportsPart2 } from "./day-2.js";
 import { memoryMulPart1, memoryMulPart2 } from "./day-3.js";
+import { wordSearchPart1, wordSearchPart2 } from "./day-4.js";
 
 console.log("day 1");
 console.log("match total (part 1):", matchTotalPart1());
@@ -13,3 +14,7 @@ console.log("safe reports (part 2):", safeReportsPart2());
 console.log("day 3");
 console.log("memory multiply (part 1):", memoryMulPart1());
 console.log("memory multiply (part 2):", memoryMulPart2());
+
+console.log("day 4");
+console.log("word search (part 1):", wordSearchPart1());
+console.log("word search (part 2):", wordSearchPart2());
