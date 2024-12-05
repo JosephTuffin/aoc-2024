58eb4523bd8598d7b7f3091fc6b7f7a703c8054a
Joseph Tuffin <joseph.tuffin@atradius.com>
2024-12-05 08:56:14 +0000

    added day 5

---
+++ src/day-5.ts
@@ -0,0 +1,69 @@
+const pairs = `47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+`;
+
+const input = `75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47`;
+
+const generateMap = (pairs: string) =>
+  pairs.split("\n").reduce((map, line) => {
+    const [key, value] = line.split("|");
+    map[key] ? map[key].push(value) : (map[key] = [value]);
+    return map;
+  }, {});
+
+const isLineValid = (line: string[], pageMap: Record<string, string[]>) =>
+  line.every(
+    (page, index, arr) =>
+      index === arr.length - 1 ||
+      arr.slice(index + 1).every((nextPage) => pageMap[page] && pageMap[page].includes(nextPage)),
+  );
+
+export const pageOrderingPart1 = () => {
+  const pageMap = generateMap(pairs);
+  const total = input.split("\n").reduce((count, line) => {
+    const lineArr = line.split(",");
+    const validLine = isLineValid(lineArr, pageMap);
+    return count + (validLine ? parseInt(lineArr[Math.round(lineArr.length - 1) / 2]) : 0);
+  }, 0);
+  return total;
+};
+
+export const pageOrderingPart2 = () => {
+  const pageMap = generateMap(pairs);
+  const total = input.split("\n").reduce((count, line) => {
+    const lineArr = line.split(",");
+    const validLine = isLineValid(lineArr, pageMap);
+    if (validLine) return count;
+    const orderedLine = lineArr.reduce((line, page, _, arr) => {
+      const filteredPages = arr.filter((otherPages) => pageMap[page] && pageMap[page].includes(otherPages));
+      line[arr.length - filteredPages.length - 1] = page;
+      return line;
+    }, new Array<string>(lineArr.length));
+    return count + parseInt(orderedLine[Math.round(orderedLine.length - 1) / 2]);
+  }, 0);
+  return total;
+};
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { matchTotalPart1, matchTotalPart2 } from "./day-1.js";
 import { safeReportsPart1, safeReportsPart2 } from "./day-2.js";
 import { memoryMulPart1, memoryMulPart2 } from "./day-3.js";
 import { wordSearchPart1, wordSearchPart2 } from "./day-4.js";
+import { pageOrderingPart1, pageOrderingPart2 } from "./day-5.js";
 
 console.log("day 1");
 console.log("match total (part 1):", matchTotalPart1());
@@ -18,3 +19,7 @@ console.log("memory multiply (part 2):", memoryMulPart2());
 console.log("day 4");
 console.log("word search (part 1):", wordSearchPart1());
 console.log("word search (part 2):", wordSearchPart2());
+
+console.log("day 5");
+console.log("page ordering (part 1):", pageOrderingPart1());
+console.log("page ordering (part 2):", pageOrderingPart2());
