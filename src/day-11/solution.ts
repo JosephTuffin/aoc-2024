f7c8876dde6e1e74b92f94bad735786c43f482f5
Joseph Tuffin <joseph.tuffin@atradius.com>
2024-12-11 11:25:37 +0000

    simplify logic

--- src/day-11/solution.ts
+++ src/day-11/solution.ts
@@ -1,8 +1,4 @@
-const getStones = (input: string) => {
-  const stones = new Map<string, number>();
-  input.split(" ").forEach((stone) => stones.set(stone, 1));
-  return stones;
-};
+const getStones = (input: string) => new Map<string, number>(input.split(" ").map((stone) => [stone, 1]));
 
 const getStonesCount = (stones: Map<string, number>) => Array.from(stones.values()).reduce((acc, count) => acc + count, 0);
 
@@ -10,21 +6,17 @@ const iterate = (stones: Map<string, number>, iterations: number) => {
   for (let i = 0; i < iterations; i++) {
     for (const [stone, count] of new Map(stones)) {
       if (stone === "0") {
-        if (stones.has("1")) stones.set("1", stones.get("1") + count);
-        else stones.set("1", count);
+        stones.set("1", (stones.get("1") || 0) + count);
         stones.set(stone, stones.get(stone) - count);
       } else if (stone.length % 2 === 0) {
         const firstHalf = parseInt(stone.slice(0, stone.length / 2)).toString();
         const secondHalf = parseInt(stone.slice(stone.length / 2)).toString();
-        if (stones.has(firstHalf)) stones.set(firstHalf, stones.get(firstHalf) + count);
-        else stones.set(firstHalf, count);
-        if (stones.has(secondHalf)) stones.set(secondHalf, stones.get(secondHalf) + count);
-        else stones.set(secondHalf, count);
+        stones.set(firstHalf, (stones.get(firstHalf) || 0) + count);
+        stones.set(secondHalf, (stones.get(secondHalf) || 0) + count);
         stones.set(stone, stones.get(stone) - count);
       } else {
         const newStone = (parseInt(stone) * 2024).toString();
-        if (stones.has(newStone)) stones.set(newStone, stones.get(newStone) + count);
-        else stones.set(newStone, count);
+        stones.set(newStone, (stones.get(newStone) || 0) + count);
         stones.set(stone, stones.get(stone) - count);
       }
     }
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { inputDay1, matchTotalPart1, matchTotalPart2 } from "./day-1/index.js";
 import { inputDay10 } from "./day-10/input.js";
 import { trailheadsPart1, trailheadsPart2 } from "./day-10/solution.js";
 import { inputDay11 } from "./day-11/input.js";
-import { stoneSplitPart1 } from "./day-11/solution.js";
+import { stoneSplitPart1, stoneSplitPart2 } from "./day-11/solution.js";
 import { inputDay2, safeReportsPart1, safeReportsPart2 } from "./day-2/index.js";
 import { inputDay3, memoryMulPart1, memoryMulPart2 } from "./day-3/index.js";
 import { inputDay4, wordSearchPart1, wordSearchPart2 } from "./day-4/index.js";
@@ -54,3 +54,4 @@ console.log("trailheads (part 2):", trailheadsPart2(inputDay10));
 
 console.log("day 11");
 console.log("stone split (part 1):", stoneSplitPart1(inputDay11));
+console.log("stone split (part 2):", stoneSplitPart2(inputDay11));
